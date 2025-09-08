db1e1e1d6472b7d2fb2f9c1562a62a31074a4151
Marine Leyvraz <marine.leyvraz@unibe.ch>
2025-09-08 16:15:41 +0200

    Test new branch

--- aera/core.py
+++ aera/core.py
@@ -36,6 +36,9 @@ def runmean(array, winlen):
         array: timeseries over which the running mean is calculated
         winlen: window length of running mean
     """
+    print('----')
+    print('Executing AERA on branch AERA_1.0')
+    print('----')
     return np.convolve(array, np.ones((winlen)) / winlen, mode='same')
 
 
